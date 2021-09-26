8eb0db83feca8745d2d8fb59f4f0ee283aac26c1
DeveloperDulli <dulli1616@kakao.com>
2021-09-26 21:48:50 +0900

    Python

---
+++ syntax/string3.py
@@ -0,0 +1,2 @@
+name = 'apple'
+print('to '+name+'. Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.liquip ex ea commodo consequat. '+name+' Duis aute irure dolor in reprehenderit apple computer in voluptate Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui '+name+' officia deserunt mollit anim id est laborum.')

4a2696c3954f1c132df687cb89f5767be3393ff7
DeveloperDulli <dulli1616@kakao.com>
2021-09-28 23:51:50 +0900

    Python

---
+++ cgi_env.py
@@ -0,0 +1,5 @@
+#!C:\Program Files\Python39\python.exe
+print("Content-Type: text/html")
+print()
+import cgi
+cgi.test()
---
+++ syntax/boolean.py
@@ -0,0 +1,25 @@
+#number
+print(1) #Integer
+
+#String
+print('Hello world')
+
+#Boolean
+print(True) #참
+print(False) #거짓
+
+#Expression
+print(1+1)
+print('Hello '+'world') #Hello world
+
+#Comparison operator
+print(1==1) #True
+print(1<2)  #True
+print(2<1)  #False
+
+#Membership operator
+print('world' in 'Hello world') #True
+
+#check exist file in directory
+import os.path
+print(os.path.exists('boolean.py'))
